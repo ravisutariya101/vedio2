49658a155d2409dbfa8aadea6db43e3709b709b7
ravisutariya101 <154221316+ravisutariya101@users.noreply.github.com>
2024-04-26 01:11:48 +0530

    last real thing a 2.54 minit

---
+++ mcq time/theRealthing.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+    char ch='a';
+    if(3 + 2 % 5)
+        printf("this works\n");
+    if(a=10)
+        printf("even this works\n");
+    if(-5)
+        printf("surprisingly even this works\n");
+    if(ch=='g')
+        printf("hello");
+    return 0;
+}
